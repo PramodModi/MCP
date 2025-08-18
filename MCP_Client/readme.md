e950bc13d8713f500028850e24c1d7a0a20f087a
Pramod Modi <pramodkmodi@gmail.com>
2025-08-18 18:04:27 +0530

    Added dummy MCP servers

---
+++ MCP_Client/readme.md
@@ -0,0 +1,2 @@
+Goal is to create MCP client.
+We will create a sort of dummy server to test the MCP client.
---
+++ MCP_Client/servers/weather_server.py
@@ -0,0 +1,11 @@
+from typing import List
+from mcp.server.fastmcp import FastMCP
+mcp = FastMCP("Weather")
+
+@mcp.tool()
+def get_weather(city: str) -> str:
+    """Get weather for a city"""
+    return f"Weather for {city} is sunny"
+
+if __name__ == "__main__":
+    mcp.run(transport='sse')
